391671c73c77f600273f63eb866486752ada8933
Amr M. Kayid <amrkayid2027@gmail.com>
2018-11-16 02:55:31 +0200

    [feat:Ctrl] create & update tasks

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -171,3 +171,126 @@ module.exports.updateToDoName = function(req, res, next) {
     });
   });
 };
+
+module.exports.createTask = function(req, res, next) {
+  if (!Validations.isObjectId(req.params.todoId)) {
+    return res.status(422).json({
+      err: null,
+      msg: 'todoId parameter must be a valid ObjectId.',
+      data: null
+    });
+  }
+  var valid =
+    req.body.description &&
+    Validations.isString(req.body.description) &&
+    (req.body.dueDate ? Validations.isDate(req.body.dueDate) : true);
+
+  if (!valid) {
+    return res.status(422).json({
+      err: null,
+      msg:
+        'description(String) is a required field, dueDate(Date) is optional but has to be a valid date, done(Boolean) is optional but has to be a valid boolean.',
+      data: null
+    });
+  }
+  User.findById(req.decodedToken.user._id).exec(function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(404)
+        .json({ err: null, msg: 'User not found.', data: null });
+    }
+
+    var todo = user.todos.id(req.params.todoId);
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ err: null, msg: 'todo not found.', data: null });
+    }
+    // Security Check
+    delete req.body.done;
+    delete req.body.createdAt;
+    delete req.body.updatedAt;
+
+    var task = todo.tasks.create(req.body);
+    todo.tasks.push(task);
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({
+        err: null,
+        msg: 'Task was created successfully.',
+        data: task
+      });
+    });
+  });
+};
+
+module.exports.updateTask = function(req, res, next) {
+  if (
+    !Validations.isObjectId(req.params.todoId) &&
+    Validations.isObjectId(req.params.taskId)
+  ) {
+    return res.status(422).json({
+      err: null,
+      msg: 'todoId and taskId parameters must be valid ObjectIds.',
+      data: null
+    });
+  }
+  var valid =
+    req.body.description &&
+    Validations.isString(req.body.description) &&
+    (req.body.dueDate ? Validations.isDate(req.body.dueDate) : true) &&
+    (req.body.done ? Validations.isBoolean(req.body.done) : true);
+  if (!valid) {
+    return res.status(422).json({
+      err: null,
+      msg:
+        'description(String) is a required field, dueDate(Date) is optional but has to be a valid date, done(Boolean) is optional but has to be a valid boolean.',
+      data: null
+    });
+  }
+  User.findById(req.decodedToken.user._id).exec(function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(404)
+        .json({ err: null, msg: 'User not found.', data: null });
+    }
+
+    var todo = user.todos.id(req.params.todoId);
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ err: null, msg: 'todo not found.', data: null });
+    }
+
+    var task = todo.tasks.id(req.params.taskId);
+    if (!task) {
+      return res
+        .status(404)
+        .json({ err: null, msg: 'Task not found.', data: null });
+    }
+    task.description = req.body.description;
+    task.dueDate = req.body.dueDate || task.dueDate;
+    task.done = req.body.done !== null ? req.body.done : task.done;
+    task.updatedAt = moment().toDate();
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({
+        err: null,
+        msg: 'Task was updated successfully.',
+        data: task
+      });
+    });
+  });
+};
