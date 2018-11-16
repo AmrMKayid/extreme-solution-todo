90fe67077c0299b4dc0b721e615eea49af7e1a80
Amr M. Kayid <amrkayid2027@gmail.com>
2018-11-16 02:49:41 +0200

    [feat:Ctrl] create todo

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -54,3 +54,56 @@ module.exports.getToDo = function(req, res, next) {
     });
   });
 };
+
+module.exports.createToDo = function(req, res, next) {
+  var valid = req.body.name && Validations.isString(req.body.name);
+  if (!valid) {
+    return res.status(422).json({
+      err: null,
+      msg: 'name is a required field.',
+      data: null
+    });
+  }
+  User.findById(req.decodedToken.user._id).exec(function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(404)
+        .json({ err: null, msg: 'User not found.', data: null });
+    }
+    var newTodoName = req.body.name.trim().toLowerCase();
+    var todoNameExists = user.todos.some(function(todo) {
+      return todo.name == newTodoName;
+    });
+    if (todoNameExists) {
+      return res.status(409).json({
+        err: null,
+        msg:
+          'A todo with the same name "' +
+          newTodoName +
+          '" already exists, please try another name.',
+        data: null
+      });
+    }
+
+    // Security Check
+    delete req.body.tasks;
+    delete req.body.createdAt;
+    delete req.body.updatedAt;
+
+    var newTodo = user.todos.create(req.body);
+    user.todos.push(newTodo);
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.status(201).json({
+        err: null,
+        msg: 'Todo was created successfully.',
+        data: newTodo
+      });
+    });
+  });
+};
